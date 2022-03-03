dff4a53a5421289679b9de506a2cf0a9c15775f0
darkapplepower <chictaehee@naver.com>
2022-03-03 23:41:05 +0900

    .

---
+++ 문제/1000/1030.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main(){
+    long long a;
+    std::cin>>a;
+    std::cout<<a<<std::endl;
+    return 0;
+}
---
+++ 문제/1000/1046.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    printf("%d\n%.1f", a+b+c, (a+b+c)/3.0);
+    return 0;
+}
